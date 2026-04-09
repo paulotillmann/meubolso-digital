7f901c9dddfbbb8caa344b803119a5f41b4d7118
Desenvolvedor <dev@meubolso.digital>
2026-04-09 13:46:58 -0300

    style: update table header and refactor entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+if (import.meta.env.DEV) {
+  import("react-grab");
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
